17108329634ca4f80dcf15e2c7e23325699a53cd
dandevri <hi@dandevri.es>
2023-10-07 14:17:02 +0200

    Add chart.js library + test component

---
+++ src/components/Bar.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { onMount } from 'svelte';
+	import Chart from 'chart.js/auto';
+
+	onMount(async () => {
+		const ctx = document.getElementById('Bar');
+
+		new Chart(ctx, {
+			type: 'bar',
+			data: {
+				labels: ['Red', 'Blue', 'Yellow', 'Green'],
+				datasets: [
+					{
+						label: '# of Votes',
+						data: [12, 19, 3, 5, 2, 3],
+						borderWidth: 1
+					}
+				]
+			},
+			options: {
+				scales: {
+					y: {
+						beginAtZero: true
+					}
+				}
+			}
+		});
+	});
+</script>
+
+<article>
+	<canvas id="Bar" />
+</article>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Bar from '../components/Bar.svelte';
+</script>
+
+<svelte:head>
+	<title>Above the Surface</title>
+	<link rel="icon" type="image/svg" href="/images/logo.svg" />
+	<link rel="icon" href="/images/logo.png" type="image/png" />
+</svelte:head>
+
+<h1>Above the Surface</h1>
+
+<main>
+	<Bar />
+</main>
